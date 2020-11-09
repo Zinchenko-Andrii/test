4eefff2dfce3aae1b08190bb209fb9822e915a51
Zinchenko-Andrii <zinchenko.andrii@airslate.com>
2020-11-09 16:45:55 +0200

    temp

--- .github/actions/DeleteBranch/src/index.js
+++ .github/actions/DeleteBranch/src/index.js
@@ -3,27 +3,13 @@ const github = require('@actions/github');
 
 (async () => {
     try {
-            const {
-                    context: {
-                            payload: { repository },
-                    },
-            } = github;
-
-        console.log('repository', repository);
-            const { data } = await github.getOctokit(process.env.GITHUB_TOKEN).request(`${repository.url}/branches`);
-
-            // const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
-        // console.log('----->>>',myToken)
-        // console.log('----->>>',JSON.stringify(Object.keys(octokit), null, 2))
-        // const data = await octokit.repos.listBranches({
-        //     owner: 'Zinchenko-Andrii',
-        //     repo: 'test',
-        // });
-
-        // const { data } = await github.getOctokit(process.env.GITHUB_TOKEN).request('/Zinchenko-Andrii/test/branches');
-
-
-        console.log('----->>>',JSON.stringify(data, null, 2))
+        const {
+                context: {
+                        payload: { repository },
+                },
+        } = github;
+        const responce = await github.getOctokit(process.env.GITHUB_TOKEN).request(`${repository.url}/branches`);
+        console.log('----->>>', responce)
 
     } catch (error) {
         core.setFailed(error.message);
