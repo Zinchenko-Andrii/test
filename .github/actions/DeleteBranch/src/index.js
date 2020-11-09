293fc3ee99b1c434354401234d4af601bc15d7f9
Zinchenko-Andrii <zinchenko.andrii@airslate.com>
2020-11-09 16:19:13 +0200

    temp

--- .github/actions/DeleteBranch/src/index.js
+++ .github/actions/DeleteBranch/src/index.js
@@ -3,14 +3,17 @@ const github = require('@actions/github');
 
 (async () => {
     try {
-        const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+        // const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
         // console.log('----->>>',myToken)
         // console.log('----->>>',JSON.stringify(Object.keys(octokit), null, 2))
-        const data = await octokit.repos.listBranches({
-            owner: 'Zinchenko-Andrii',
-            repo: 'test',
-        });
-        //
+        // const data = await octokit.repos.listBranches({
+        //     owner: 'Zinchenko-Andrii',
+        //     repo: 'test',
+        // });
+
+        const { data } = await github.getOctokit(process.env.GITHUB_TOKEN).request('Zinchenko-Andrii/test/branches');
+
+
         console.log('----->>>',JSON.stringify(data, null, 2))
 
     } catch (error) {
