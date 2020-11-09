8ee945d166a670eb29ae345de77f78910e670412
Zinchenko-Andrii <zinchenko.andrii@airslate.com>
2020-11-09 17:17:41 +0200

    temp

--- .github/actions/DeleteBranch/src/index.js
+++ .github/actions/DeleteBranch/src/index.js
@@ -8,20 +8,25 @@ const github = require('@actions/github');
 
         const octokit = github.getOctokit(process.env.GITHUB_TOKEN,)
 
-        // const { data } = await octokit.repos.listBranches({
-        //     owner: owner.name,
-        //     repo: name,
-        //     protected: false,
-        // });
-        //
-        //  console.log('----->>>', JSON.stringify(data, null, 2))
-
-        let info = await octokit.repos.getBranch({
+        const { data } = await octokit.repos.listBranches({
             owner: owner.name,
             repo: name,
-            branch: '12',
-        })
-        console.log('!!!!', info);
+            protected: false,
+        });
+
+         console.log('----->>>', JSON.stringify(data, null, 2))
+
+        for (let branch in data) {
+            console.log('--', branch)
+            console.log('--', data[branch].name)
+        }
+
+        // let info = await octokit.repos.getBranch({
+        //     owner: owner.name,
+        //     repo: name,
+        //     branch: '12',
+        // })
+        // console.log('!!!!', info);
 
     } catch (error) {
         core.setFailed(error.message);
