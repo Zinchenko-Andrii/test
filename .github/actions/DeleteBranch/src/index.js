07510050e15eb8e7bebaf7deaffbbe90c95f1dcf
Zinchenko-Andrii <zinchenko.andrii@airslate.com>
2020-11-09 15:46:31 +0200

    temp

--- .github/actions/DeleteBranch/src/index.js
+++ .github/actions/DeleteBranch/src/index.js
@@ -3,7 +3,8 @@ const github = require('@actions/github');
 
 (async () => {
     try {
-        console.log(JSON.stringify(core))
+        console.log(JSON.stringify(core, null, 2))
+        console.log(JSON.stringify(github, null, 2))
     } catch (error) {
         core.setFailed(error.message);
     }
