6c2d5b5154c82045d42ad1e18b1babfc062de3ff
Zinchenko-Andrii <zinchenko.andrii@airslate.com>
2020-11-09 17:54:02 +0200

    temp

--- .github/actions/DeleteBranch/src/index.js
+++ .github/actions/DeleteBranch/src/index.js
@@ -1,44 +1,43 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+const { name, owner } = github.context.payload.repository;
+const defaultCreds = { owner: owner.name, repo: name };
+
+const deleteBranch = (name) => (
+    octokit.git.deleteRef({
+        ...github.context.repo,
+        ref: `heads/${name}`
+    })
+)
+
+const getBranchList = (list) => list.reduce((acc, { name }) => (
+    [
+        ...acc,
+        octokit.repos.getBranch({
+            ...defaultCreds,
+            branch: name,
+        }),
+    ]
+), [])
+
 (async () => {
     try {
-        const { name, owner } = github.context.payload.repository;
-        const defaultCreds = { owner: owner.name, repo: name };
-        const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+        octokit.repos.listBranches({ ...defaultCreds, protected: false,})
+            .then(({ data }) => {
+                Promise.all(
+                    getBranchList(data)
+                ).then((list) => {
+                    const branches = list.reduce((acc, { data }) => {
+                        const { name, commit: { commit: { author, committer}} } = data;
+                        return ([ ...acc, { name, author, committer } ])
+                    }, [])
 
-        // console.log(JSON.stringify(octokit, null, 2))
-
-        const res = await octokit.git.deleteRef({
-            ...github.context.repo,
-            ref: "heads/12"
-        })
-
-        console.log(JSON.stringify(res));
-        // const getBranchList = (list) => list.reduce((acc, { name }) => (
-        //     [
-        //         ...acc,
-        //         octokit.repos.getBranch({
-        //             ...defaultCreds,
-        //             branch: name,
-        //         }),
-        //     ]
-        // ), [])
-        //
-        // octokit.repos.listBranches({ ...defaultCreds, protected: false,})
-        //     .then(({ data }) => {
-        //
-        //         Promise.all(
-        //             getBranchList(data)
-        //         ).then((list) => {
-        //             const branches = list.reduce((acc, { data }) => {
-        //                 const { name, commit: { commit: { author, committer}} } = data;
-        //                 return ([ ...acc, { name, author, committer } ])
-        //             }, [])
-        //
-        //             console.log(JSON.stringify(branches, null, 2));
-        //         })
-        //     })
+                    console.log(JSON.stringify(branches, null, 2));
+                })
+            })
     } catch (error) {
         core.setFailed(error.message);
     }
