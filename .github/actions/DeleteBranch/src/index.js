1f6397e9481263dea98b62fa0c594041ebefe3e6
Zinchenko-Andrii <zinchenko.andrii@airslate.com>
2020-11-09 15:59:03 +0200

    temp

--- .github/actions/DeleteBranch/src/index.js
+++ .github/actions/DeleteBranch/src/index.js
@@ -3,8 +3,8 @@ const github = require('@actions/github');
 
 (async () => {
     try {
-        const myToken = core.getInput('${{ secrets.GITHUB_TOKEN }}');
-        // const octokit = github.getOctokit(myToken)
+        const myToken = core.getInput('4f42bf708e037ae3c1d6ebf26375dde521c8bcb4');
+        const octokit = github.getOctokit(myToken)
         console.log('----->>>',myToken)
         console.log('----->>>',octokit)
     } catch (error) {
