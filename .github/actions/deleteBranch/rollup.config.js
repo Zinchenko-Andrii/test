e975f22da9f7980979e658785ea9d8fc1b540514
Zinchenko-Andrii <zinchenko.andrii@airslate.com>
2020-11-11 14:50:18 +0200

    temp

---
+++ .github/actions/deleteBranch/rollup.config.js
@@ -0,0 +1,1 @@
+module.exports = require('@airslate/platform-dev-tools/rollup.config')({ ci: true });
---
+++ .github/actions/deleteBranch/src/index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const _http = require('@actions/http-client');
+
+const api = require('./api');
+const { DELETE_DAY, NOTIFY_DAY } = require('./constants');
+const { createNotificationBody } = require('./utils');
+
+(async () => {
+  try {
+    const branches = await api.getBranches();
+
+    if (branches.length) {
+      const http = new _http.HttpClient();
+      console.log(JSON.stringify(branches, null, 2));
+
+      // on Notify_Day(Monday) - action will only notify about next deletion session
+      if (new Date().getDay() === NOTIFY_DAY) {
+        console.log('---->>>   notify');
+        await http.post(
+          process.env.SLACK_HOOK_URL,
+          JSON.stringify(
+            createNotificationBody(branches, false),
+          ),
+        );
+      }
+
+      // on Delete_Day(Thursday) - action will delete all deprecated branch and notify about it.
+      if (new Date().getDay() === DELETE_DAY) {
+        console.log('---->>>   deletion');
+        await http.post(
+          process.env.SLACK_HOOK_URL,
+          JSON.stringify(
+            createNotificationBody(branches, true),
+          ),
+        );
+        await api.deleteBranchList(branches);
+      }
+    } else {
+      console.log('---->>>   nothing to do');
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
